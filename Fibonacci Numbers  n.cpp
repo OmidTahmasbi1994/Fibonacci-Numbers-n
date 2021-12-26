e3c816525730fd29755c67ba38ab00c61f64cca8
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-27 02:28:02 +0330

    Add files via upload

---
+++ Fibonacci Numbers  n.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main()
+
+{
+	long i;
+	cout<<"Enter a Positive Number:"<<endl;
+	cin>>i;
+	cout<<"Fibonacci Numbers < "<<i<<" : \n 0 , 1";
+	
+	long f0=0;
+	long f1=1;
+	
+	while(true)
+	{
+		long f2 = f0 + f1 ;
+		
+		if(f2>i) 
+		break;
+		
+		cout<<" , "<<f2;
+		
+		f0=f1;
+		f1=f2;
+		
+		
+	}
+}
